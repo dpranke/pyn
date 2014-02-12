3a69b88fefc7655cb67eaa1311b733ec8b7423be
Dirk Pranke <dpranke@chromium.org>
2014-02-11 16:11:05 -0800

    clean up grammar

--- pyn.py
+++ pyn.py
@@ -44,7 +44,9 @@ def parse_args(argv):
 
     parser = argparse.ArgumentParser()
     parser.usage = 'pyn [options] [targets...]'
-    parser.description = 'if targets are unspecified, builds the \'%s\' target (see manual)' % DEFAULT_TARGET
+    parser.description = (
+            'if targets are unspecified, builds the \'%s\' '
+            'target (see manual)' % DEFAULT_TARGET)
     parser.add_argument('--version', action='store_true',
         help='print pyn version ("%s")' % VERSION)
     parser.add_argument('-C', metavar='DIR', dest='dir',
@@ -52,7 +54,8 @@ def parse_args(argv):
     parser.add_argument('-f', metavar='FILE', default='build.ninja',
         help='specify input build file [default=%(default)s]')
     parser.add_argument('-j', default=multiprocessing.cpu_count(), metavar='N',
-        help='run N jobs in parallel [default=%(default)s, derived from CPUs available]')
+        help=('run N jobs in parallel [default=%(default)s, '
+              'derived from CPUs available]'))
     parser.add_argument('-l', metavar='N',
         help='do not start new jobs if the load average is greater than N')
     parser.add_argument('-k', metavar='N', default=1,
@@ -76,51 +79,54 @@ class NinjaParser(pymeta_helper.ParserBase):
 
 grammar    = decls:ds end -> ds
 
-decls      = spaces_or_comments decl:d  decls:ds -> [d] + ds
-           | spaces_or_comments decl:d           -> [d]
-           | spaces_or_comments                  -> []
+decls      = ws_or_comments decl:d  decls:ds -> [d] + ds
+           | ws_or_comments decl:d           -> [d]
+           | ws_or_comments                  -> []
 
-decl       = rule | build_edge | variable | default | reference | pool
+decl       = rule | build | var | default | subninja | import | pool
 
-rule       = "rule" spaces ident:n eol indented_var+:vs -> ['rule', n, vs]
+rule       = "rule" ws ident:n eol indented_var+:vs -> ['rule', n, vs]
 
-build_edge = "build" spaces paths:os spaces ":" spaces ident:rule spaces paths:ins "|" spaces targets:ts eol -> ['build', os, rule, ins, ts]
-build_edge = "build" spaces paths:os spaces ":" spaces ident:rule spaces paths:ins eol -> ['build', os, rule, ins, []]
+build      = "build" ws paths:os ws ":" ws ident:rule ws paths:ins deps:ds eol
+           -> ['build', os, rule, ins, ds]
 
-variable   = ident:n spaces "=" spaces values:v eol -> ['var', n, v]
+deps       = "|" targets:ts -> ts
+           | -> []
 
-values     = (~eol anything)+:v  -> ''.join(v)
+var        = ident:n ws "=" spaces (~eol anything)+:v eol
+           -> ['var', n, ''.join(v)]
 
-default    = "default" spaces targets:ts -> ['default', ts]
+default    = "default" ws targets:ts -> ['default', ts]
 
-reference  = "subninja" spaces path:p  -> ['subninja', p]
-           | "include" spaces path:p   -> ['import', p]
+subninja   = "subninja" ws path:p    -> ['subninja', p]
 
-pool       = "pool" spaces ident:name eol indented_var+:vars -> ['pool', name, vars]
+import     = "include" ws path:p     -> ['import', p]
 
-indented_var = indent variable:v -> v
+pool       = "pool" ws ident:name eol indented_var+:vars -> ['pool', name, vars]
 
-targets    = ident:i spaces targets:ts -> [i] + ts
-           | ident:i                   -> [i]
+indented_var = ws var:v              -> v
 
-indent     = (' '|'\t')+
-
-spaces_or_comments = (' '|'\t'|('$' '\n')|'\n'|comment)*
-
-comment    = "#" ~('\n') '\n'
-
-eol        = comment
-           | ' '* ~('$' '\n') '\n'
+targets    = ident:i ws targets:ts   -> [i] + ts
+           | ident:i                 -> [i]
 
 ident      = (letter|'_'|'$'):hd (letter)*:tl -> ''.join([hd] + tl)
 
-paths      = path:p spaces paths:ps -> [p] + ps
+paths      = path:p ws paths:ps -> [p] + ps
            | path:p -> [p]
 
 path       = '"' (~('"'|'\n') anything)+:p '"' -> ''.join(p)
            | (~(' '|':'|'='|eol) anything)+:p -> ''.join(p)
-"""
 
+eol        = comment
+           | ' '* ~('$' '\n') '\n'
+
+comment    = "#" (~'\n' anything)* '\n'
+
+ws         = ' '+
+
+ws_or_comments = (' '|('$' '\n')|'\n'|comment)*
+
+"""
 
 if __name__ == '__main__':
     main()
