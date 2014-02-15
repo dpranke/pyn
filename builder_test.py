6a43ec00e34861d27cc70e821b4b11b40cbd3b7f
Dirk Pranke <dpranke@chromium.org>
2014-02-14 17:02:55 -0800

    add more test skeletons

---
+++ builder_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import unittest
+
+from builder import Builder
+
+
+class TestBuilder(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ main_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import unittest
+
+import main
+
+
+class TestMain(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
