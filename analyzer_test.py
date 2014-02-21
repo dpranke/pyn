9bc400c6a743d23ca174dde1a966e371e6a99b53
Dirk Pranke <dpranke@chromium.org>
2014-02-21 09:34:07 -0800

    add more tests for semantic errors

--- analyzer_test.py
+++ analyzer_test.py
@@ -15,16 +15,10 @@ class TestAnalyzer(unittest.TestCase):
         self.parse = lambda msg: []
         self.expand_vars = lambda v, scope: v
 
-    def assertGraphsEqual(self, a, b):
-        self.assertEqual(a, b)
-        self.fail()
-
-    def check(self, ast, expected_graph=None):
+    def check(self, ast):
         analyzer = NinjaAnalyzer(self.host, self.args, self.parse,
                                  self.expand_vars)
-        actual_graph = analyzer.analyze(ast, 'build.ninja')
-        if expected_graph:
-            self.assertGraphsEqual(actual_graph, expected_graph)
+        analyzer.analyze(ast, 'build.ninja')
 
     def err(self, ast):
         analyzer = NinjaAnalyzer(self.host, self.args, self.parse,
@@ -34,11 +28,18 @@ class TestAnalyzer(unittest.TestCase):
     def test_empty(self):
         self.check([])
 
+    def test_build(self):
+        self.check([['build', ['foo.o'], 'cc', ['foo.c'], [], [], []]])
+        self.err([['build', ['foo.o'], 'cc', ['foo.c'], [], [], []],
+                    ['build', ['foo.o'], 'cc', ['foo.c'], [], [], []]])
+        self.err([['build', ['foo.o'], 'cc', ['foo.c'], [], [],
+                   [['var', 'foo', 'bar'],
+                    ['var', 'foo', 'bar']]]])
+
     def test_vars(self):
         self.check([['var', 'foo', 'bar']])
-
-    def test_pool(self):
-        self.check([['pool', 'foo', [['var', 'depth', 4]]]])
+        self.err([['var', 'foo', 'bar'],
+                  ['var', 'foo', 'bar']])
 
     def test_subninja_missing(self):
         self.err([['subninja', 'missing.ninja']])
@@ -54,6 +55,26 @@ class TestAnalyzer(unittest.TestCase):
     def test_import_missing(self):
         self.err([['include', 'missing.ninja']])
 
+    def test_pool(self):
+        self.check([['pool', 'foo', [['var', 'depth', 4]]]])
+        self.err([['pool', 'foo', [['var', 'depth', 'foo']]]])
+        self.err([['pool', 'foo', [['var', 'depth', 4]]],
+                  ['pool', 'foo', [['var', 'depth', 4]]]])
+        self.err([['pool', 'foo', []]])
+        self.err([['pool', 'foo', [['var', 'foo', 4]]]])
+        self.err([['pool', 'foo', [['var', 'depth', 4],
+                                   ['var', 'foo', 'bar']]]])
+
+    def test_rule(self):
+        self.check([['rule', 'cc', [['var', 'command', 'cc -o $out $in']]]])
+
+        self.err([['rule', 'cc', [['var', 'command', 'cc -o $out $in']]],
+                  ['rule', 'cc', [['var', 'command', 'cc -o $out $in']]]])
+        self.err([['rule', 'cc', [['var', 'command', 'cc -o $out $in'],
+                                  ['var', 'command', 'touch $out']]]])
+
+    def test_default(self):
+        self.check([['default', ['all']]])
 
 if __name__ == '__main__':
     unittest.main()
