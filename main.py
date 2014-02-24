13441b57d969cb2a2ff27cbd0dcda91c806e447d
Dirk Pranke <dpranke@chromium.org>
2014-02-23 16:50:57 -0800

    tweak clean target to handle generated files properly by default

--- main.py
+++ main.py
@@ -94,18 +94,21 @@ def main(host, argv=None):
 
 
 def clean(host, args, graph):
-    outputs = [n.name for n in list(graph.nodes.values())
-               if n.rule_name != 'phony' and host.exists(n.name)]
+    files_to_remove = []
+    for output_name, node in graph.nodes.items():
+        if (node.rule_name != 'phony' and host.exists(output_name) and
+            (node.scope['generator'] != '1' or '-g' in args.targets)):
+            files_to_remove.append(output_name)
     if args.verbose:
         host.print_err('Cleaning...')
     else:
         host.print_err('Cleaning... ', end='')
-    for o in outputs:
+    for f in files_to_remove:
         if args.verbose:
-            host.print_err('Remove %s' % o)
+            host.print_err('Remove %s' % f)
         if not args.dry_run:
-            host.remove(o)
-    host.print_err('%d files.' % len(outputs))
+            host.remove(f)
+    host.print_err('%d files.' % len(files_to_remove))
     return 0
 
 
